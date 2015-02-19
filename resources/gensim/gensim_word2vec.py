f6ebd6b2c749376244b1f3ea91af30dbd7b33975
David Przybilla <dav.alejandro@gmail.com>
2015-02-19 14:08:29 +0000

    adding gensim word2vec script

---
+++ resources/gensim/gensim_word2vec.py
@@ -0,0 +1,56 @@
+import multiprocessing
+import os
+from optparse import OptionParser
+
+import gensim
+
+os.system("taskset -p 0xff %d" % os.getpid())
+
+
+def read_corpus(path_to_corpus, output_path, min_count=10, size=500, window=10):
+    workers = multiprocessing.cpu_count()
+    sentences = gensim.models.word2vec.LineSentence(path_to_corpus)
+    model = gensim.models.Word2Vec(sentences, min_count=min_count, size=size,
+                                   window=window, sg=1, workers=workers)
+    model.save(output_path)
+
+
+def main():
+    parser = OptionParser(usage="usage: %prog [options] corpus outputModel",
+                          version="%prog 1.0")
+
+    parser.add_option("-m", "--min_count",
+                      action="store",
+                      dest="min_count",
+                      default=10,
+                      type="int",
+                      help="min number of apperances",)
+
+    parser.add_option("-s", "--size",
+                      action="store",
+                      dest="size",
+                      default=500,
+                      type="int",
+                      help="vectors size",)
+
+    parser.add_option("-w", "--windows",
+                      action="store",
+                      dest="window",
+                      default=10,
+                      type="int",
+                      help="window size",)
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 2:
+        parser.error("wrong number of arguments")
+
+    option_dict = vars(options)
+    option_dict["path_to_corpus"] = args[0]
+    option_dict["output_path"] = args[1]
+
+    read_corpus(**option_dict)
+
+
+if __name__ == "__main__":
+    main()
---
+++ resources/gensim/wiki2vec.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#+------------------------------------------------------------------------------------------------------------------------------+
+#| Idio Wiki2Vec                                                                      |                                                                                                     |
+#+------------------------------------------------------------------------------------------------------------------------------+
+
+# Creates Wiki2Vec vectors out of massaged wikipedia corpus
+# It uses gensim
+
+CORPUS=$1
+OUPUTFOLDER=$2
+MIN_COUNT=$3
+VECTOR_SIZE=$4
+WINDOW_SIZE=$5
+
+# Install all the python dependencies
+#apt-get install python-pip build-essential liblapack-dev gfortran zlib1g-dev python-dev
+#pip install cython
+#apt-get install python-numpy python-scipy
+#pip install gensim
+
+# Call python script
+python gensim_word2vec.py $CORPUS $OUPUTFOLDER -m $MIN_COUNT -s $VECTOR_SIZE -w $WINDOW_SIZE

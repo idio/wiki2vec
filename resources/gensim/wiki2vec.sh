58e7d958921762128f119f67e085be65f050051b
David Przybilla <dav.alejandro@gmail.com>
2015-02-19 14:34:07 +0000

    adding gensim dependencies

--- resources/gensim/wiki2vec.sh
+++ resources/gensim/wiki2vec.sh
@@ -13,10 +13,10 @@ VECTOR_SIZE=$4
 WINDOW_SIZE=$5
 
 # Install all the python dependencies
-#apt-get install python-pip build-essential liblapack-dev gfortran zlib1g-dev python-dev
-#pip install cython
-#apt-get install python-numpy python-scipy
-#pip install gensim
+apt-get install python-pip build-essential liblapack-dev gfortran zlib1g-dev python-dev
+pip install cython
+apt-get install python-numpy python-scipy
+pip install gensim
 
 # Call python script
 python gensim_word2vec.py $CORPUS $OUPUTFOLDER -m $MIN_COUNT -s $VECTOR_SIZE -w $WINDOW_SIZE

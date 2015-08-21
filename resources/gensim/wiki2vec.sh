2023960935821739aeec09e0b9038568010049d2
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-08-21 16:25:19 +0200

    add ENTITY_MIN_COUNT option to wiki2vec.sh

--- resources/gensim/wiki2vec.sh
+++ resources/gensim/wiki2vec.sh
@@ -7,10 +7,11 @@
 # It uses gensim
 
 CORPUS=$1
-OUPUTFOLDER=$2
+OUTPUTFOLDER=$2
 MIN_COUNT=$3
-VECTOR_SIZE=$4
-WINDOW_SIZE=$5
+ENTITY_MIN_COUNT=$4
+VECTOR_SIZE=$5
+WINDOW_SIZE=$6
 
 # Install all the python dependencies
 apt-get install python-pip build-essential liblapack-dev gfortran zlib1g-dev python-dev
@@ -19,4 +20,4 @@ apt-get install python-numpy python-scipy
 pip install gensim
 
 # Call python script
-python gensim_word2vec.py $CORPUS $OUPUTFOLDER -m $MIN_COUNT -s $VECTOR_SIZE -w $WINDOW_SIZE
+python gensim_word2vec.py $CORPUS $OUTPUTFOLDER -m $MIN_COUNT -e $ENTITY_MIN_COUNT -s $VECTOR_SIZE -w $WINDOW_SIZE
